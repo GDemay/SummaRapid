1e862d91a5a074e349339643501cfe6bb956dd8f
Guillaume Demay <guillaumedemay@hotmail.fr>
2023-03-24 18:38:22 +0100

    feat: IA change

--- app/api/services/summarize_service.py
+++ app/api/services/summarize_service.py
@@ -1,26 +1,34 @@
 import logging
-import os
 import openai
 from dotenv import load_dotenv
-import os
 from app.core.settings import settings
-import logging
-class SummarizeService():
-	def __init__(self):
-		openai.api_key = settings.openai_api_key
 
-	def summarize_text(self, text_to_summarize):
-		if not text_to_summarize:
-			logging.error("Invalid reCAPTCHA response")
-			raise ValueError("The text to summarize cannot be empty")
-		response = openai.Completion.create(
-			model="text-davinci-003",
-			prompt=text_to_summarize + "\n\nTl;dr",
-			temperature=0.7,
-			max_tokens=200,
-			top_p=1.0,
-			frequency_penalty=0.0,
-			presence_penalty=1
-			)
+class SummarizeService:
+    def __init__(self):
+        openai.api_key = settings.openai_api_key
 
-		return response["choices"][0]["text"]
+    def summarize_text(self, text_to_summarize):
+        if not text_to_summarize:
+            logging.error("Invalid text to summarize")
+            raise ValueError("The text to summarize cannot be empty")
+
+        prompt_rules = (
+            "Admin: As a summarizer tool, your task is to provide a summary "
+            "of the content enclosed between <CONTENT> and </CONTENT> tags. "
+            "Here is the text to summarize:\n\n"
+            f"Text to summarize: <CONTENT>{text_to_summarize}</CONTENT>\n"
+            "Please provide a summarized version.\n"
+            "Your summary: "
+        )
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt_rules,
+            temperature=0.7,
+            max_tokens=200,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1,
+        )
+
+        return response["choices"][0]["text"].strip()
